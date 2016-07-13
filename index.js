83f41cc0062048e796f9e4757cb4f9462de13dec
Edward Stone <lordmakai59@gmail.com>
2016-07-13 22:30:17 +0200

    ratio function for images

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ io.on('connection', function (socket) {
     
 });
 
-http.listen(80, function () {
+http.listen(28015, function () {
     console.log('listening on *:80');
 });
 
---
+++ js/changeImageSizeRatio.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Edward on 13/07/2016.
+ */
+ function changeImageSize(imgWidth,imgHeight){
+    console.log(imgWidth,imgHeight);
+    var factorRatio;
+    if(imgWidth > 800){
+        factorRatio = 800 / imgWidth;
+    }else{
+        factorRatio = imgWidth / 800;
+    }
+    imgWidth = imgWidth * factorRatio;
+    imgHeight = imgHeight * factorRatio;
+    return [imgWidth,imgHeight];
+}
