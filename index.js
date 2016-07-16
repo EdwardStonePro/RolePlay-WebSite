142cbd2d78c74318b7970ff6ee36238cda560580
Edward Stone <lordmakai59@gmail.com>
2016-07-16 19:34:29 +0200

    Changing some css

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var path = require('path');
 var ChatPerson = require('./chatClasses');
 var checkFunctions = require('./checkFunctions');
 var associative_array_chatPerson = {};
 var logChat = [];
 
-app.use(express.static(__dirname+ 'public'));
-app.use(express.static(__dirname+'js'));
-app.use(express.static(__dirname+'css'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
